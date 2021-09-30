b0716326becc4f541356773bda0fe9d316fe2fbe
Sebastien MICHEL <sebastien.t.michel@renault.com>
2021-09-30 16:24:36 +0200

    My current state
    Signed-off-by: Sebastien MICHEL <sebastien.t.michel@renault.com>

--- data.js
+++ data.js
@@ -1,42 +1,93 @@
 data = {
-    "start":"id4",
+    "start":"id1",
     "persons": {
-        "id1": { "id": "id1", "name": "Adam", "birthyear": 1900, "deathyear": 1980, "own_unions": ["u1"], "birthplace":"Alberta", "deathplace":"Austin"},
-        "id2": { "id": "id2", "name": "Berta", "birthyear": 1901, "deathyear": 1985, "own_unions": ["u1"], "birthplace":"Berlin", "deathplace":"Bern" },
-        "id3": { "id": "id3", "name": "Charlene", "birthyear": 1930, "deathyear": 2010, "own_unions": ["u3", "u4"], "parent_union": "u1", "birthplace":"Château", "deathplace":"Cuxhaven" },
-        "id4": { "id": "id4", "name": "Dan", "birthyear": 1926, "deathyear": 2009, "own_unions": [], "parent_union": "u1", "birthplace":"den Haag", "deathplace":"Derince" },
-        "id5": { "id": "id5", "name": "Eric", "birthyear": 1931, "deathyear": 2015, "own_unions": ["u3"], "parent_union": "u2", "birthplace":"Essen", "deathplace":"Edinburgh" },
-        "id6": { "id": "id6", "name": "Francis", "birthyear": 1902, "deathyear": 1970, "own_unions": ["u2"], "birthplace":"Firenze", "deathplace":"Faizabad" },
-        "id7": { "id": "id7", "name": "Greta", "birthyear": 1905, "deathyear": 1990, "own_unions": ["u2"] },
-        "id8": { "id": "id8", "name": "Heinz", "birthyear": 1970, "own_unions": ["u5"], "parent_union": "u3" },
-        "id9": { "id": "id9", "name": "Iver", "birthyear": 1925, "deathyear": 1963, "own_unions": ["u4"] },
-        "id10": { "id": "id10", "name": "Jennifer", "birthyear": 1950, "own_unions": [], "parent_union": "u4" },
-        "id11": { "id": "id11", "name": "Klaus", "birthyear": 1933, "deathyear": 2013, "own_unions": [], "parent_union": "u1" },
-        "id12": { "id": "id12", "name": "Lennart", "birthyear": 1999, "own_unions": [], "parent_union": "u5" },
+        "id1": { "name": "Yves Mille / Perit", "birthyear":"9 oct. 1922" },
+        "id2": { "name": "Helene Garnier / Mamie"},
+        "id3": { "name": "Bon papa\nEtienne"},
+        "id4": { "name": "Bonne maman\nBernadette"},
+        "id5": { "name": "Philippe"},
+        "id6": { "name": "Manell"},
+        "id7": { "name": "Francois\nMichel"},
+        "id8": { "name": "Rose France"},
+        "id9": { "name": "Robert / Tadou"},
+
+        "id10": { "name": "Dominique", "birthyear": "20 octobre 1943", "birthplace":"La fleche"},
+        "id11": { "name": "Nicolas"},
+        "id12": { "name": "Pascal"},
+        "id13": { "name": "Jean-Francois"},
+        "id14": { "name": "Frederique"},
+        "id15": { "name": "Claire Michel"},
+        "id16": { "name": "Luc"},
+
+        "id20": { "name": "George"},
+        "id21": { "name": "Francoise", "birthyear": "16 aout 1948",},
+        "id22": { "name": "Anne"},
+        "id23": { "name": "Edith"},
+        "id24": { "name": "Claire"},
+        "id25": { "name": "Isabelle"},
+        "id26": { "name": "Benedicte"},
+
+        "id30": { "name": "Florence", "birthyear": "9 dec. 1974", "birthplace":"Castres" },
+        "id31": { "name": "Sebastien", "birthyear": "14 fev. 1979", "birthplace":"Castres"},
+        "id32": { "name": "David",     "birthyear": "14 fev. 1979", "birthplace":"Castres"},
+        "id33": { "name": "Raphael",   "birthyear": "2 jan. 1984", "birthplace":"Castelnaudary"},
+
+        "id40": { "name": "Eloise",    "birthyear": "25 juin 2008"},
+        "id41": { "name": "Chloé",     "birthyear": "3 mars 2011"},
+
+        "id50": { "name": "Gilles"},
+        "id51": { "name": "Soizik", "birthyear": 1950 },
+
+        "id60": { "name": "Veronique / Maya"},
+        "id61": { "name": "Blandine"},
+        "id62": { "name": "Thomas"},
+
+        "id70": { "name": "Marie-Joseph / Madou"},
+        "id71": { "name": "Benoit", "birthyear": "11 Jan. 1945", "deathyear": 2013, "deathplace":"Puy de dome" },
+
+        "id80": { "name": "David", "birthyear": "", "birthplace":""},
+        "id81": { "name": "Bastien", "birthyear": "1973", "birthplace":""},
+        "id82": { "name": "Celine", "birthyear": "20 oct. 1977", "birthplace":"La fleche"},
+
+        "id90": { "name": "Yann", "birthyear": "", "birthplace":""},
+        "id91": { "name": "Thibault", "birthyear": "", "birthplace":""},
+        "id92": { "name": "Fanny", "birthyear": "", "birthplace":""},
+        "id93": { "name": "Manon", "birthyear": "", "birthplace":""},
+
+        "id100": { "name": "Leo", "birthyear": "", "birthplace":""},
+        "id101": { "name": "Martin", "birthyear": "", "birthplace":""},
+        "id102": { "name": "Felix", "birthyear": "", "birthplace":""},
+
+        "id1000": { "name": "Benoit de Premorel"},
+        "id1001": { "name": "Dominique"},
     },
     "unions": {
-        "u1": { "id": "u1", "partner": ["id1", "id2"], "children": ["id3", "id4", "id11"] },
-        "u2": { "id": "u2", "partner": ["id6", "id7"], "children": ["id5"] },
-        "u3": { "id": "u3", "partner": ["id3", "id5"], "children": ["id8"] },
-        "u4": { "id": "u4", "partner": ["id3", "id9"], "children": ["id10"] },
-        "u5": { "id": "u5", "partner": ["id8"], "children": ["id12"] },
-    },
-    "links": [
-        ["id1", "u1"],
-        ["id2", "u1"],
-        ["u1", "id3"],
-        ["u1", "id4"],
-        ["id6", "u2"],
-        ["id7", "u2"],
-        ["u2", "id5"],
-        ["id3", "u3"],
-        ["id5", "u3"],
-        ["u3", "id8"],
-        ["id3", "u4"],
-        ["id9", "u4"],
-        ["u4", "id10"],
-        ["u1", "id11"],
-        ["id8", "u5"],
-        ["u5", "id12"],
-    ]
+        // mille
+        "u1": { "partner": ["id1", "id2"], "children": ["id20", "id21", "id22", "id23", "id24", "id25", "id26"] },
+        //michel
+        "u2": { "partner": ["id7", "id8"], "children": ["id10", "id11", "id12", "id13", "id14", "id15", "id16"] },
+        "u3": { "partner": ["id10", "id21"], "children": ["id30", "id31", "id32", "id33"] },
+        "u4": { "partner": ["id31", "id82"], "children": ["id40", "id41"] },
+
+        // chabbert
+        "u5": { "partner": ["id5", "id6"], "children": ["id50", "id51"] },
+        "u6": { "partner": ["id51", "id71"], "children": ["id80", "id81", "id82"] },
+
+        // froidevaux
+        "u7": { "partner": ["id3", "id4"], "children": ["id70", "id71"]},
+
+        // guerard
+        "u8": { "partner": ["id70", "id9"], "children": ["id60", "id61", "id62"]},
+        "u9": { "partner": ["id60", "id1000"], "children": ["id90", "id91", "id92"]},
+        "u10": { "partner": ["id61", "id1001"], "children": ["id100", "id101", "id102"]},
+
+        "u11": { "partner": ["id60"], "children":["id21"], "relationship":"linkother", "name": "babysitter" },
+        "u12": { "partner": ["id7", "id3"], "relationship":"linkother", "name": "pharmacie" },
+        "u13": { "partner": ["id20", "id10" ], "relationship":"linkother", "name": "copain" },
+        "u14": { "partner": ["id20", "id71" ], "relationship":"linkother", "name": "copain" },
+        "u15": { "partner": ["id20", "id51" ], "relationship":"linkother", "name": "compagnon" },
+        "u16": { "partner": ["id24", "id61" ], "relationship":"linkother", "name": "copine" },
+        "u17": { "partner": ["id62", "id25" ], "relationship":"linkother", "name": "copain" },
+        "u18": { "partner": ["id2", "id6" ], "relationship":"linkother", "name": "amies" },
+    }
 }
